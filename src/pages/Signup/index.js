03e5664c750c2356a828d0b88dc0a7a39096944e
LeoAlveslcm <leoalveslcm@gmail.com>
2023-06-02 03:27:17 -0300

    use hook form in SignUp

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -5,18 +5,38 @@ import {
   Text, 
   StyleSheet,
   TouchableOpacity,
+  TextInput,
 } from 'react-native'
 
-import InputSign from '../../components/Inputs/InputSign'
-
+import * as yup from 'yup'
 import * as Animatable from 'react-native-animatable'
+
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { useNavigation } from '@react-navigation/native'
+
+const schema = yup.object({
+  name: yup.string().required("Informe seu nome"),
+  lastName: yup.string().required("Informe seu sobrenome"),
+  email: yup.string().email("Email Invalido").required("Informe seu email"),
+  number: yup.string().required("Informe seu telefone"),
+  user: yup.string().required("Informe seu usuário"),
+  password: yup.string().min(6, "Asenha deve ter pelo menos 6 digitos").required("Informe sua senha"),
+})
 
 export default function SignUn() {
   const navigation = useNavigation();
   const [isSelected, setSelected] = useState(false)
   const [isChecked, setChecked] = useState(false)
 
+  const { control, handleSubmit, formState: { errors }} = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  function handleSignIn(data) {
+    console.log(data);
+  }
+  
     return (
       <View style={styles.container}>
 
@@ -24,12 +44,91 @@ export default function SignUn() {
           <View style={styles.inputGroup}>
             <View style={{gap: 15}}>
               
-              <InputSign place= "Nome"/>
-              <InputSign place= "Sobrenome"/>
-              <InputSign place= "E-mail"/>
-              <InputSign place= "Telefone"/>
-              <InputSign place= "Usuário"/>
-              <InputSign place= "Senha" secure={true}/>
+              <Controller 
+                control={control}
+                name="name"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput 
+                    style={styles.input}
+                    placeholder= "Nome"
+                    onChangeText={onChange}
+                    value={value}
+                    />
+                )}
+              />
+              {errors.name && <Text style={styles.error}>{errors.name?.message}</Text>}
+              
+              <Controller 
+                control={control}
+                name="lastName"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput 
+                    style={styles.input}
+                    placeholder= "Sobrenome"
+                    onChangeText={onChange}
+                    value={value}
+                    />
+                )}
+              />
+              {errors.lastName && <Text style={[styles.error, styles.error2]}>{errors.lastName?.message}</Text>}
+
+              <Controller 
+                control={control}
+                name="email"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput 
+                    style={styles.input}
+                    placeholder= "E-mail"
+                    onChangeText={onChange}
+                    value={value}
+                    />
+                )}
+              />
+              {errors.email && <Text style={[styles.error, styles.error3]}>{errors.email?.message}</Text>}
+
+              <Controller 
+                control={control}
+                name="number"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput 
+                    style={styles.input}
+                    placeholder= "Telefone"
+                    onChangeText={onChange}
+                    value={value}
+                    />
+                )}
+              />
+              {errors.number && <Text style={[styles.error, styles.error4]}>{errors.number?.message}</Text>}
+
+              <Controller 
+                control={control}
+                name="user"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput 
+                    style={styles.input}
+                    placeholder= "Usuário"
+                    onChangeText={onChange}
+                    value={value}
+                    />
+                )}
+              />
+              {errors.user && <Text style={[styles.error, styles.error5]}>{errors.user?.message}</Text>}
+
+              <Controller 
+                control={control}
+                name="password"
+                render={({ field: { onChange, value } }) => (
+                  <TextInput 
+                    style={styles.input}
+                    placeholder= "Senha"
+                    secureTextEntry={true}
+                    onChangeText={onChange}
+                    value={value}
+                    />
+                )}
+              />
+              {errors.password && <Text style={[styles.error, styles.error6]}>{errors.password?.message}</Text>}
+
             </View>
 
             <View style={styles.checkView}>
@@ -58,7 +157,7 @@ export default function SignUn() {
 
             <TouchableOpacity 
             style={[styles.btnCadastrar]}   
-            onPress={ () => navigation.navigate('Home')}>
+            onPress={handleSubmit(handleSignIn)}>
               <Text style={{fontSize: 18, color: '#fff'}}>Cadastrar</Text>
             </TouchableOpacity>
           </View>
@@ -93,7 +192,7 @@ export default function SignUn() {
 
       alignItems: 'center',
       justifyContent: 'flex-start',
-      gap: 15,
+      gap: 20,
 
       backgroundColor: '#ffffff',
       borderRadius: 10,
@@ -111,6 +210,7 @@ export default function SignUn() {
     },
 
     btnCadastrar:{
+      bottom: 10,
       width: 300,
       height: 45,
       backgroundColor: '#00275b',
@@ -118,5 +218,38 @@ export default function SignUn() {
 
       justifyContent: 'center',
       alignItems: 'center', 
+    },
+
+    input:{
+      height: 40,
+      width: 300,
+      borderBottomWidth: 0.5,
+    },
+
+    error:{
+      color: 'red',
+      fontSize: 12,
+      position: 'absolute',
+      top: 40
+    },
+
+    error2:{
+      top: 95
+    },
+
+    error3:{
+      top: 150
+    },
+
+    error4:{
+      top: 205
+    },
+    
+    error5:{
+      top: 260
+    },
+    
+    error6:{
+      top: 315
     },
   })
